df8634cb5a344ee9d19e1821fa652dadb26fdfcd
anggariansah <34121270+anggariansah@users.noreply.github.com>
2020-05-11 20:01:49 +0700

    First commit

---
+++ app/src/main/java/tik/pnj/laporanodp/MainActivity.java
@@ -0,0 +1,14 @@
+package tik.pnj.laporanodp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
