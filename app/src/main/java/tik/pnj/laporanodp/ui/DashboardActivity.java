c4b1ecbb1a25fdf4d9d681102bb33993023593ee
anggariansah <34121270+anggariansah@users.noreply.github.com>
2020-05-12 15:05:28 +0700

    Added Change Password'

--- app/src/main/java/tik/pnj/laporanodp/ui/DashboardActivity.java
+++ app/src/main/java/tik/pnj/laporanodp/ui/DashboardActivity.java
@@ -1,16 +1,55 @@
 package tik.pnj.laporanodp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import tik.pnj.laporanodp.R;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener  {
+
+    private CardView mCardProfile, mCardLaporan, mCardChangePassword, mCardAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        mCardProfile = findViewById(R.id.cv_profile);
+        mCardLaporan = findViewById(R.id.cv_laporan);
+        mCardChangePassword = findViewById(R.id.cv_ganti_password);
+        mCardAbout = findViewById(R.id.cv_tentang);
+
+        mCardProfile.setOnClickListener(this);
+        mCardLaporan.setOnClickListener(this);
+        mCardChangePassword.setOnClickListener(this);
+        mCardAbout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cv_profile:
+
+                break;
+
+            case R.id.cv_laporan:
+
+                break;
+
+            case R.id.cv_ganti_password:
+
+                Intent intentPassword = new Intent(DashboardActivity.this, ChangePasswordActivity.class);
+                startActivity(intentPassword);
+
+                break;
+
+            case R.id.cv_tentang:
+
+                break;
+        }
     }
 }
