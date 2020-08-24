ef77c8c31ee70033abc5326a9f7c21d6ce807047
Alvin Tandiardi <alvintandiardi@gmail.com>
2020-08-24 20:34:46 +0700

    Change Item Laporan

--- app/src/main/java/tik/pnj/laporanodp/ui/laporan/listlaporan/ListLaporanAdapter.java
+++ app/src/main/java/tik/pnj/laporanodp/ui/laporan/listlaporan/ListLaporanAdapter.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import tik.pnj.laporanodp.R;
@@ -39,17 +41,22 @@ public class ListLaporanAdapter extends RecyclerView.Adapter<ListLaporanAdapter.
 
         LaporanEntity model = listLaporan.get(position);
 
-        int hari = position + 1;
-        holder.mTvHari.setText(String.valueOf(hari));
-        holder.mTvTanggal.setText(model.getTanggal());
-        holder.mTvStatus.setText("SUDAH LAPOR");
+        int day = position + 1;
+        SimpleDateFormat formatFromDb = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
 
-        holder.mBtnDetail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                itemClickListener.onItemClick(v, position);
-            }
-        });
+        try {
+            date = formatFromDb.parse(model.getTanggal());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        SimpleDateFormat formatDisplay = new SimpleDateFormat("EEEE, dd MMMM y");
+        String dateConvert = formatDisplay.format(date);
+
+        holder.mTvHari.setText(context.getResources().getString(R.string.laporan_ke, String.valueOf(day)));
+        holder.mTvTanggal.setText(dateConvert);
+
 
     }
 
@@ -60,16 +67,13 @@ public class ListLaporanAdapter extends RecyclerView.Adapter<ListLaporanAdapter.
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView mTvHari, mTvTanggal, mTvStatus;
-        Button mBtnDetail;
+        TextView mTvHari, mTvTanggal;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             mTvHari = itemView.findViewById(R.id.tv_item_hari);
             mTvTanggal = itemView.findViewById(R.id.tv_item_date);
-            mTvStatus = itemView.findViewById(R.id.tv_item_status);
-            mBtnDetail = itemView.findViewById(R.id.btn_item_detail);
         }
     }
 
